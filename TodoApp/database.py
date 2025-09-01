80d45e624fb35d081e4942a41546e63eb324ffbd
kuldeep7688 <7688kuldeepsingh@gmail.com>
2025-09-01 13:04:58 -0400

    added endpoints

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -8,7 +8,8 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     # we will only allow one thread to communicate with SQL
     # assuming that each thread handles an independent request
-    # to prevent any type of accidental sharing of the same connection between different request
+    # to prevent any type of accidental sharing of the same connection between
+    # different request
     # since there could be multiple threads happening towards the same database
     # so sqllite should check multiple threads
     connect_args={"check_same_thread": False}
